213b46fac31555fa4534ba41eb4cd5d575418fe5
wbdana <william.b.dana@gmail.com>
2017-07-18 13:52:52 -0400

    WIP

--- index.js
+++ index.js
@@ -10,12 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {};
+  newItem[item] = Math.floor(Math.random()*(100-1+1)+1);
+  cart.push(newItem);
+  console.log(`${item} has been added to your cart.`);
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  var last = cart[cart.length -1];
+  var i = 0;
+  if(parseInt(cart.length) === 0) {
+    console.log('Your shopping cart is empty.')
+  } else if (parseInt(cart.length) === 1) {
+    console.log(`In your cart, you have ${cart[0][0]} at $${cart[0][1]}.`)
+  } else {
+    var str = "In your cart, you have "
+    for (i; i < parseInt(cart.length) - 2; i++) {
+      str.push(`${cart[i][0]} at $${cart[i][1]}, `)
+    }
+    str.push(`and ${last[0]} at $${last[1]}.`)
+    console.log(str)
+  }
 }
+
 
 function total() {
   // write your code here
